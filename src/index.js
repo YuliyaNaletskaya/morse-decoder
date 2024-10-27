09e3263df9bc96142dc2fc3cb85a962077f0fde9
YuliyaNaletskaya <naletskaya@gmail.com>
2024-10-27 20:16:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   let arr = [];
+   for (let i = 0; i < expr.length; i += 10) {
+    let letter = expr.substring(i, (i + 10));
+    let ind = letter.indexOf(1);
+    if (ind > 0) {
+      letter = letter.substring(ind);
+    }
+    let word = letter.replaceAll('10', '.');
+    let word1 = word.replaceAll('11', '-');
+    let word2 = word1.replace('**********', ' ');
+    
+    arr.push(word2);
+   }
+   let morz = [];
+   for (let el = 0; el < arr.length; el++) {
+    if (arr[el] === ' ') {
+      morz.push(arr[el]);
+    } else {
+     let str = MORSE_TABLE[arr[el]];
+      morz.push(str);
+    }
+   }
+  let phrase = morz.join('');
+  return phrase;// write your solution here
 }
 
 module.exports = {
